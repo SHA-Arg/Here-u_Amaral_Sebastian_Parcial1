ea547d84c3c885ed57ae5e7b37abc8a245863f97
SHA-Arg <hereniu.amaral.sebastian@gmail.com>
2024-10-18 08:43:58 -0300

    upload enunciado y readme

---
+++ H_Amaral_Sebastian_Parcial1/src/h_amaral_sebastian_parcial1/H_Amaral_Sebastian_Parcial1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package h_amaral_sebastian_parcial1;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class H_Amaral_Sebastian_Parcial1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
